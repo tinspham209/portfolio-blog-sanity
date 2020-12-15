3d2350af47512325e2b6a6b6c8beecfb938b7500
tinspham209 <tindeptrai1@gmail.com>
2020-12-16 01:41:43 +0700

    export component

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Home } from "./Home";
+export { default as About } from "./About";
+export { default as Navbar } from "./Navbar";
+export { default as Post } from "./Post";
+export { default as Project } from "./Project";
+export { default as SinglePost } from "./SinglePost";
